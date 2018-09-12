21017feb6296fdbea68618f09bc8118f6883deeb
LiangYutao <sinothk@126.com>
2018-09-12 15:27:50 +0800

    添加发布依赖！

--- app/src/main/java/com/sinothk/widget/mallImageView/demo/MainActivity.java
+++ app/src/main/java/com/sinothk/widget/mallImageView/demo/MainActivity.java
@@ -2,6 +2,11 @@ package com.sinothk.widget.mallImageView.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.sinothk.widget.mallImageView.MallImageView;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] urls = {
+                "https://img.alicdn.com/imgextra/i4/113462038/TB2y0NFfMLD8KJjSszeXXaGRpXa_!!113462038.jpg",
+                "https://img.alicdn.com/imgextra/i4/113462038/TB2rbXYfL6H8KJjSspmXXb2WXXa_!!113462038.jpg",
+                "https://img.alicdn.com/imgextra/i4/113462038/TB2rbXYfL6H8KJjSspmXXb2WXXa_!!113462038.jpg",
+                "https://img.alicdn.com/imgextra/i2/113462038/TB2NUXDfRDH8KJjSspnXXbNAVXa_!!113462038.jpg",
+                "https://img.alicdn.com/imgextra/i2/113462038/TB2NUXDfRDH8KJjSspnXXbNAVXa_!!113462038.jpg",
+                "https://img.alicdn.com/imgextra/i4/113462038/TB2rENDfRTH8KJjy0FiXXcRsXXa_!!113462038.jpg",
+                "https://img.alicdn.com/imgextra/i2/113462038/TB2ZZNwfL2H8KJjy0FcXXaDlFXa_!!113462038.jpg",
+                "https://img.alicdn.com/imgextra/i1/113462038/TB2rmRSfPnD8KJjSspbXXbbEXXa_!!113462038.jpg",
+                "https://img.alicdn.com/imgextra/i2/113462038/TB2rupFfMLD8KJjSszeXXaGRpXa_!!113462038.jpg"
+
+        };
+
+        MallImageView mallDetailImageView = (MallImageView) this.findViewById(R.id.mallDetailView);
+        mallDetailImageView.setImgUrls(urls);
+        mallDetailImageView.setImgUrls(Arrays.asList(urls));
+        mallDetailImageView.setOnImgLongClickListener(new MallImageView.OnImgLongClickListener() {
+            @Override
+            public void imgLongClick(int position, String imgUrl) {
+                Log.e("MallDemoImageView",imgUrl);
+            }
+        });
     }
 }
