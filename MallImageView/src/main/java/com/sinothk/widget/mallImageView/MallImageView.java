81078e27949b04fb3470400c207c5976e64d6035
LiangYT <381518188@qq.com>
2020-08-14 15:21:31 +0800

    调整参数！

--- MallImageView/src/main/java/com/sinothk/widget/mallImageView/MallImageView.java
+++ MallImageView/src/main/java/com/sinothk/widget/mallImageView/MallImageView.java
@@ -3,12 +3,13 @@ package com.sinothk.widget.mallImageView;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
-import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
 import com.bumptech.glide.Glide;
 
--- app/src/main/java/com/sinothk/widget/mallImageView/demo/MainActivity.java
+++ app/src/main/java/com/sinothk/widget/mallImageView/demo/MainActivity.java
@@ -1,8 +1,9 @@
 package com.sinothk.widget.mallImageView.demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.sinothk.widget.mallImageView.MallImageView;
 
